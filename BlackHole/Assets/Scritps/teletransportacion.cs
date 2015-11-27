309a644d23220c5bbc57fde322a375594b6bee07
forni19 <afp.forni@gmail.com>
2015-11-27 20:35:21 +0100

    [28]teletransporte_2

---
+++ BlackHole/Assets/Scritps/teletransportacion.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teletransportacion : MonoBehaviour {
+
+	private GameObject destino;  //GameObject 
+	public bool activo = false;
+	public string nombre;
+	 public bool dentro = false; 
+	bool pulsado; 
+	GameObject player;
+	void Awake (){
+		destino= GameObject.Find(nombre);// buscando de mis objetos hijos 
+		player= GameObject.Find("Jugador");
+	}
+
+	void Update(){
+
+		if (Input.GetKeyDown (KeyCode.W) && dentro) {
+			player.transform.position= destino.transform.position;
+			Debug.Log("tecla pulsada");
+
+		}
+
+
+
+	}
+	void OnTriggerStay2D (Collider2D col){  //se activa cuando algo se colisiona 
+		if (col.transform.tag == "player"  ) {
+			dentro= true;
+			Debug.Log (destino.transform.name);
+
+		}
+
+	}
+	
+	void OnTriggerExit2D (Collider2D col){  //se activa cuando algo se colisiona 
+		if (col.transform.tag == "player"  ) {
+
+			dentro= false;
+			
+		}
+		
+	}
+	void OnDrawGizmos(){
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(transform.position,destino.transform.position);
+		
+		
+	}
+	
+
+	
+
+
+
+}
