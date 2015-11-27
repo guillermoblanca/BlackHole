437fb8653ed82072e18bf106387557cc79674f63
guillermoblanca <merlockes@gmail.com>
2015-11-28 00:12:35 +0100

    [19] movimiento
    debe arreglarse ciertas cosas como la velocidad constante que tiene  ya
    que genera ciertos bugs de movimiento.
    
    - falta salto

--- BlackHole/Assets/Scritps/MovimientoPersonaje.cs
+++ BlackHole/Assets/Scritps/MovimientoPersonaje.cs
@@ -3,13 +3,56 @@ using System.Collections;
 
 public class MovimientoPersonaje : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[Range (0,10)] public float velocidad = 10f;
+	public float fuerzaSalto;
+	public bool enSalto;
+	public bool miraIzq;
+
+	CircleCollider2D detectorSuelo;
+
+	Rigidbody2D rg;
+	void Awake()
+	{
+		detectorSuelo = GameObject.Find ("ComprobadorSuelo").GetComponent<CircleCollider2D>();
+		rg= GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update() // Controles
+	{
+		if (Input.GetKey(KeyCode.A)) // izq
+		{
+			MoverIzq(velocidad);
+		}
+		if (Input.GetKeyUp(KeyCode.A)) //izq
+		{
+			MoverIzq(0);
+		}
+		if (Input.GetKey(KeyCode.D))// drcha
+		{
+			MoverDrch(velocidad);
+		}
+		if (Input.GetKeyUp(KeyCode.D))// drcha
+		{
+			MoverDrch(0);
+		}
+		if (Input.GetKey(KeyCode.Space))
+		{
+			rg.AddForce (new Vector2(0,fuerzaSalto));
+		}
 	}
+	void OnCollisionEnter2D (Collision2D objeto)
+	{
+		if (detectorSuelo)
+		{
+			Debug.Log ("toco algo");
+		}
+	}
+	void MoverIzq(float velocidad)
+	{
+		rg.velocity = new Vector2 (-velocidad, 0);
+	}
+	void MoverDrch(float velocidad)
+	{
+		rg.velocity = new Vector2(velocidad,0);
+	}
+
 }
