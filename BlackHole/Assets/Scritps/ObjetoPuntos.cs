4b38bd2bbff869945ec83637dffe251ed22b855c
Guille <staticbangs@gmail.com>
2015-11-27 19:52:06 +0100

    mejoras programacion

---
+++ BlackHole/Assets/Scritps/ObjetoPuntos.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjetoPuntos : MonoBehaviour {
+
+void OnCollisionEnter2D (Collision2D objeto)
+	{
+		if (objeto.transform.tag == "Player")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
