c14d12d5bf0968f20c7f76e0cbed3bb037a02c6e
Torrehon <albtorpa93@gmail.com>
2015-11-28 14:52:56 +0100

    cambios interfaz

--- BlackHole/Assets/Scripts/INTERFAZ/botonNuevaPartida.cs
+++ BlackHole/Assets/Scripts/INTERFAZ/botonNuevaPartida.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class botonNuevaPartida : MonoBehaviour {
 
-	Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		anim =  GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
