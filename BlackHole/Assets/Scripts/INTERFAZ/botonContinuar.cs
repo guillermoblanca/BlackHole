092f288392c3fb2078fa054e76eea163b180d978
Torrehon <albtorpa93@gmail.com>
2015-11-28 13:02:59 +0100

    menu inicial completo y sprite enemigo

---
+++ BlackHole/Assets/Scripts/INTERFAZ/botonContinuar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class botonContinuar : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim =  GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	public void cargaContinuar ()
+	{
+		
+		anim.SetBool ("confirmar", true);
+		Debug.Log ("Pulsado");
+	}
+	public void sacaContinuar ()
+	{
+		
+		anim.SetBool ("confirmar", false);
+		Debug.Log ("Pulsado");
+	}
+}
