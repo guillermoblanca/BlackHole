97536906996cd1d0e16f5074ed5851f339310bc2
Torrehon <albtorpa93@gmail.com>
2015-11-28 19:52:18 +0100

    Menu Opciones... COMPLETO

---
+++ BlackHole/Assets/Scripts/INTERFAZ/entraPausa.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class entraPausa : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim =  GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			anim.SetBool ("pausa", true);
+		}
+
+	}
+}
---
+++ BlackHole/Assets/Scripts/INTERFAZ/fueraPausa.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fueraPausa : MonoBehaviour {
+
+	public Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		anim =  GetComponent<Animator>();
+	}
+	
+
+	public void sacaPausa ()
+	{
+		
+		anim.SetBool ("pausa", false);
+		Debug.Log ("Pulsado");
+	}
+}
