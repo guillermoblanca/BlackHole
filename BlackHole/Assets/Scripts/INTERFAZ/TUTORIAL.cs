5d3971b197cf2965ec11084dbcf64db0bdf5cadb
Torrehon <albtorpa93@gmail.com>
2015-11-29 14:41:32 +0100

    TUTORIAL TO CREMA

---
+++ BlackHole/Assets/Scripts/INTERFAZ/TUTORIAL.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TUTORIAL : MonoBehaviour {
+
+	public GameObject sig1, sig2,sig3,sig4,emp;
+
+	
+
+
+	public void Siguiente1 (){
+	
+		sig1.SetActive (false);
+		sig2.SetActive (true);
+	}
+
+	public void Siguiente2 (){
+
+		sig2.SetActive (false);
+		sig3.SetActive (true);
+	
+	}
+
+	public void Siguiente3 (){
+	
+		sig3.SetActive (false);
+		sig4.SetActive (true);
+	}
+
+	public void Siguiente4 (){
+		sig4.SetActive (false);
+		emp.SetActive (true);
+	}
+
+	public void empezar (){
+
+		emp.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
