6b4234b67d7f2dc06c2775bbdb493afac0b51721
guillermoblanca <merlockes@gmail.com>
2016-06-17 14:36:03 +0200

    objeto clave
    creado script de objeto clave
    
    puesto requisitos de componentes
    - boxcollider2d
    -audiosource
    
    mensaje de aviso al diseñadorde necesidad de vincular el clip de sonido
    con el nombre
    
    quita el render del objeto para permitir el sonido del audio y elimina
    el objeto al siguiente segundo para evitar consumir recursos, al quitar
    el objeto se vuelve verdadera la opcion de objeto clave

---
+++ BlackHole/Assets/NEW/01_SCRIPTS/ObjetoClave.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof(BoxCollider2D))]
+[RequireComponent (typeof(AudioSource))]
+public class ObjetoClave : MonoBehaviour {
+	private BoxCollider2D boxcol;
+	private SpriteRenderer comp_ren;
+	private AudioSource comp_audio;
+	public AudioClip objetoclavesonido;
+	void Awake(){
+		name = "objetoClave";
+		boxcol = GetComponent<BoxCollider2D> ();
+		comp_audio = GetComponent<AudioSource> ();
+		comp_ren = GetComponent<SpriteRenderer> ();
+		boxcol.isTrigger = true;
+		if (objetoclavesonido == null) {
+			Debug.Log("Falta clip de sonido en : " +name);
+		}
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.name == "Player") {
+			comp_audio.PlayOneShot (objetoclavesonido,0.5f);
+			comp_ren.enabled = false;
+
+			Destroy (gameObject,1f);
+		}
+	}
+}
