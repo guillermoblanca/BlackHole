72a01c48a169f4733eafc4db6dbd533184e2cfc5
guillermoblanca <merlockes@gmail.com>
2016-08-24 22:52:32 +0200

    mejorado orden de carpetas y script cambio de color
    script cambio de color ahora contiene un componente tipo array para
    almacenar el color

--- BlackHole/Assets/NEW/Scripts/Engine/ZonaCambioColor.cs
+++ BlackHole/Assets/NEW/Scripts/Engine/ZonaCambioColor.cs
@@ -7,11 +7,18 @@ public class ZonaCambioColor : MonoBehaviour {
 	private SpriteRenderer comp_render;
 	CH_Movimiento personaje;
 
-	private Color rojo = new Color (0.698f,0.212f,0.125f);
-	private Color verde =  new Color (0.671f , 1f, 0.259f);
-	private Color azul= new Color (0.145f,0.482f,0.698f);
 	private BoxCollider2D col;
+
+	private static Color[] color_colores = {
+		new Color(1f,1f,1f),
+		new Color (0.698f,0.212f,0.125f), //rojo
+		new Color (0.671f , 1f, 0.259f),// verde
+		new Color (0.145f,0.482f,0.698f)// azul
+	};
+
+
 	void Start(){
+		
 		col = GetComponent<BoxCollider2D> ();
 		col.isTrigger = true;
 		personaje = GameObject.Find("Player").GetComponent<CH_Movimiento> ();
@@ -20,18 +27,19 @@ public class ZonaCambioColor : MonoBehaviour {
 	void Update(){
 		Color ();
 	}
+
 	void Color(){
 		if (colorObjeto == Colores.Gris) {
-			comp_render.color = new Color (1f, 1f, 1f);
+			comp_render.color = color_colores[0];
 		}
 		if (colorObjeto == Colores.Rojo) {
-			comp_render.color = rojo;
+			comp_render.color = color_colores [1];
 		}
 		if (colorObjeto == Colores.Verde) {
-			comp_render.color = verde;
+			comp_render.color = color_colores[2];
 		}
 		if (colorObjeto == Colores.Azul) {
-			comp_render.color = azul;
+			comp_render.color = color_colores[3] ;
 		}
 	}
 	void OnTriggerEnter2D(Collider2D obj){
--- BlackHole/Assets/NEW/Scripts/Object/ObjetoClave.cs
+++ BlackHole/Assets/NEW/Scripts/Object/ObjetoClave.cs
@@ -4,7 +4,7 @@ using System.Collections;
 [RequireComponent (typeof(AudioSource))]
 public class ObjetoClave : MonoBehaviour {
 	private BoxCollider2D boxcol;
-	private SpriteRenderer comp_ren;
+	private SpriteRenderer comp_render;
 	private AudioSource comp_audio;
 	public AudioClip objetoclavesonido;
 
@@ -12,16 +12,17 @@ public class ObjetoClave : MonoBehaviour {
 		name = "objetoClave";
 		boxcol = GetComponent<BoxCollider2D> ();
 		comp_audio = GetComponent<AudioSource> ();
-		comp_ren = GetComponent<SpriteRenderer> ();
+		comp_render = GetComponent<SpriteRenderer> ();
 		boxcol.isTrigger = true;
 		if (objetoclavesonido == null) {
 			Debug.Log("Falta clip de sonido en : " +name);
 		}
 	}
 	void OnTriggerEnter2D(Collider2D col){
+		// debe sustituirse esto en el futuro con un sound manager 
 		if (col.name == "Player") {
 			comp_audio.PlayOneShot (objetoclavesonido,0.5f);
-			comp_ren.enabled = false;
+			comp_render.enabled = false;
 			Destroy (gameObject,1f);
 		}
 	}
